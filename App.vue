dc621548bfa8b88e4f2571fddffa6809e50b238f
juvenal13 <106524212+juvenal13@users.noreply.github.com>
2023-08-16 16:03:55 +0200

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="vue-app">
+    <AssemblyLine :stages='[
+        "Idea", 
+        "Development", 
+        "Testing", 
+        "Deployment"
+      ]' 
+    />
+  </div>
+</template>
+
+<script>
+import AssemblyLine from './components/AssemblyLine.vue'
+
+export default {
+    name: 'vue-app',
+    computed: {
+    },
+    components: {
+        AssemblyLine
+    }
+}
+</script>
+
+<style>
+#vue-app {
+  font-family: arial;
+}
+
+.example {
+  padding: 1rem;
+  background-color: #f8f8f8;
+  margin-bottom: 1rem;
+}
+.h2 {
+    margin-top: 0;
+    font-size: 16px;
+  }
+</style>
